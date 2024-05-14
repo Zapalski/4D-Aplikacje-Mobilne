e71f7d86c9bf83a06ec8fb79b3c7782dfdb04e7d
Zapalski <113010429+Zapalski@users.noreply.github.com>
2024-05-14 11:20:31 +0200

    Add files via upload
    LogowanieApp

---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LogowanieApp
+{ 
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnSubmitClicked(object sender, EventArgs e)
+        {
+            string email = EmailEntry.Text;
+            string password1 = PasswordEntry1.Text;
+            string password2 = PasswordEntry2.Text;
+
+            if (string.IsNullOrWhiteSpace(email) || !email.Contains("@"))
+            {
+                MessageLabel.Text = "Nieprawidłowy adres e-mail";
+            }
+
+            else if (string.IsNullOrWhiteSpace(password1) || password1 != password2)
+            {
+                MessageLabel.Text = "Hasła się różnią";
+            }
+            else
+            {
+                MessageLabel.Text = $"Witaj {email}";
+            }
+        }
+    }
+}
